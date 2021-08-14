4b1ceac4fc2447cb054320ffb35dda516ec9ad54
Mahbub Ferdous Bijoy <87741958+Mahbub-Ferdous@users.noreply.github.com>
2021-08-14 21:42:41 +0600

    Add files via upload

---
+++ Vs_Code_tutorial.js
@@ -0,0 +1,13 @@
+Command Palete  -------   Ctrl+P    [ its too easy to find any file on command Palete ]
+
+In command palete if you write > then command any extension
+
+your javascript code and instant out put extension  -----  Quokka.js
+
+for html extension ------- Auto rename tag
+
+Find Symbols with @ in Command Palate  ----- if you want to mark any line or hint point or class then press Ctrl+F then write the hints and then again go to this hints line easyly by using command palate and write @ then show you hints
+
+Find symbols with # in command Palate  ----- 
+
+
