94f55dfc59cc3ae27584861f1c70ed0b43d0392a
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2021-08-14 21:48:50 +0600

    Correction

---
+++ setup_pycharm.py
@@ -0,0 +1,72 @@
+## Pycharm is the best IDE for python dev cause its build in for python and the only python . it also allow HTMl, CSS, Javascript, SQl
+
+
+lets talk about install and setup:
+
+1.  Install pycharm in linux(ubuntu) == Go to your termnal and just command ---- sudo snap install pycharm-community --classic
+
+							simply its take some time to install pychar yin your machine be cool
+
+2. if you open the project file   press ----- Alt+1
+
+3. all kind of setting is here and you will learn step by step when you working with pycharm 
+
+4. Debugging : when we write code then we check it because will it run properly sometime its run properly some time we get some error, when
+   get the error then we need to debug our code so how can we debug our code in pycharm :
+   
+                               click and get red icon besides the line number(called break point)
+                                                           *
+                                                           *
+                                                           *
+                                                           *
+                                              then simply click debug icon 
+                                                           *
+                                                           *
+                                                           *
+                                            then you see whats the problem
+                                                           *
+                                                           *
+                                                           *
+                                                           *
+                                            you can debug your code line by line
+                                                           *
+                                                           *
+                                                           *
+                                          and the red marker is a stop point
+                                          
+                                          
+5. always if anything need then go to help and there have search bar otherwise direct open search bar then Ctrl+shift+A then you search
+   anything search bar suggest the desire shortcut 
+   
+   
+   
+  
+6. The Basic editing : ---- 
+
+	a. Ctrl+C and Ctrl+V   Normal Copy and paste command everyone knows
+	b. if you want to duplicate a line then press Ctrl+d
+	c. if you want to delete full line then press Ctrl+y
+	d. if you want to delete line undo restore then press ------------- Ctrl+z
+	e. if you want to select single word then -------- Ctrl+w, then again press ------ Ctrl+w the whole line will be selected
+	f. if you want to cut any line then press ------------------- Ctrl+x
+	g. if you want to down running line and go to upperline and also lower line then press ---------- Ctrl+Alt+enter 
+	h. if you complete your syntax then command  ---------- Ctrl+shift+enter then auto complete the syntax
+	
+
+
+	
+   
+7. you create your TODO: list and see them after done your work jusr write TODO: taskName 
+                                               
+                                               # TODO: debug line 21 and 22 
+                                               #TODO: again test code
+
+8. if you want to formatting your code then simply press Ctrl+Alt+L
+
+9. if you want to create a new file then press --------  Ctrl+Alt+insert
+
+10. if you gonna be debuging then find something then press -------  Ctrl+f  
+
+11. For testing your code and classes and logical part then create test file by pressing ---------- Ctrl+shift+t
+
+                      ************************************************************
